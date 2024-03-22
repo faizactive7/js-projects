b4f04d46f65899215df968236d7014f2f05f486d
Mohammad Faiz Khan <147690383+faizactive7@users.noreply.github.com>
2024-03-22 05:26:21 +0000

    variable and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 145358
+let accountEmail = "hello@gmail.com"
+var accountPassword = "1233445"
+accountCity = "jaipur"
+let accountState
+// accountId = 2
+
+accountEmail="hc@ch.com"
+accountPassword="12121"
+accountCity = "selhi"
+
+console.log(accountId);
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";// treat all js code as newer version
+
+// alert(3+3) // we are using node js not browser
+
+let name = "john"
+let age = 18
+let isLoggedIn = false
+let state = null
+let accountState; //undefined
+
+// number
+// string
+// boolean
+// null->standalone value
+// undefined
+// symbol-> unique , mostly used in react
+
+// object
+console.log(typeof null); //is an object
+console.log(typeof undefined); // is undefined
