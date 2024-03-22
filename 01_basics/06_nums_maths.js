f376a9590b7585abd1b1b133307a0f36f9a43f11
Mohammad Faiz Khan <147690383+faizactive7@users.noreply.github.com>
2024-03-22 11:31:16 +0000

    half done

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,11 @@
+const score = 400
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1)); 
+
+const otherNumber = 123.8043
+
+console.log(otherNumber.toPrecision(3));
+console.log(otherNumber.toPrecision(4));
